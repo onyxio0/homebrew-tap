0afb01b244bf232e9d96957bcde4d8811385a86f
onyxio0 <17881618+onyxio0@users.noreply.github.com>
2025-12-11 11:53:38 +0000

    Update code-switch to v2.2.2

--- Casks/code-switch.rb
+++ Casks/code-switch.rb
@@ -1,13 +1,13 @@
 cask "code-switch" do
-  version "2.2.0"
+  version "2.2.2"
   
   on_arm do
-    sha256 "b7c5a84692c849b10cf20837852c0dd55b58eb3dcc8e9eda91b758dd6623dc12"
+    sha256 "2372b53334f86f10298d2e65e1d7a17618bbc392df19c6970f1a0ab1569eb743"
     url "https://github.com/Rogers-F/code-switch-R/releases/download/v#{version}/codeswitch-macos-arm64.zip"
   end
   
   on_intel do
-    sha256 "02b1ad2821f3ee7f2bec7c856a7517b9c55454ac7b9b2b25c57c64b64651dd4a"
+    sha256 "1db47955757d13ce07f94493db2353421189f7c2b973043374086e475681968a"
     url "https://github.com/Rogers-F/code-switch-R/releases/download/v#{version}/codeswitch-macos-amd64.zip"
   end
 
